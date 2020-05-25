88cd8e2105cba7b0daf432270bcee193e4fb0fd8
Ian A <n01384067@unf.edu>
2020-05-24 21:47:52 -0400

    day 3 finished

--- db/index.js
+++ db/index.js
@@ -6,6 +6,7 @@ const { Client } = require("pg"); // imports the pg module
 const client = new Client(
   process.env.DATABASE_URL || "postgres://localhost:5432/juicebox-dev"
 );
+
 /**
  * USER Methods
  */
--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 
 // EVERYTHING ELSE
 
-const PORT = 3000;
+const { PORT = 3000 } = process.env;
 const express = require("express");
 const server = express();
 
